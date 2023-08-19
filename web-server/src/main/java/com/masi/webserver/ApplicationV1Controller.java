dfbe06b5361644c863273e05370f2bd6708568dd
nidongara <16006011+nidongara@users.noreply.github.com>
2023-08-19 17:18:03 -0500

    feat: HelloWorldController

---
+++ web-server/src/main/java/com/masi/webserver/ApplicationV1Controller.java
@@ -0,0 +1,10 @@
+package com.masi.webserver;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1")
+public class ApplicationV1Controller {
+
+}
